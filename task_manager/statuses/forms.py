ddfc982aa13d7c1aa49ad033c29994e8b3a38793
ADrone <aleksandron1997@gmail.com>
2025-08-25 16:50:35 +0300

    fix: change templates

--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -4,12 +4,6 @@ from .models import Status
 
 
 class StatusForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
-
     class Meta:
         model = Status
         fields = ['name']

284eba3ccecffdd0edbb42ffda1b396284349f3c
ADrone <aleksandron1997@gmail.com>
2025-08-25 15:34:22 +0300

    fix: localization

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -6,28 +6,75 @@ User = get_user_model()
 
 
 class UserCreateForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
-
+    password1 = forms.CharField(
+        required=True,
+        label="Пароль",
+        widget=forms.PasswordInput,
+        help_text="Ваш пароль должен содержать как минимум 3 символа."
+    )
+    password2 = forms.CharField(
+        required=True,
+        label="Подтверждение пароля",
+        widget=forms.PasswordInput,
+        help_text="Для подтверждения введите, пожалуйста, пароль ещё раз."
+    )
+    
     class Meta:
         model = User
-        fields = ['username', 'first_name',
-                  'last_name', 'password1', 'password2']
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'password1',
+            'password2'
+        ]
+        labels = {
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+            'username': 'имя пользователя',
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={'required': True}),
+            'last_name': forms.TextInput(attrs={'required': True}),
+            'username': forms.TextInput(attrs={'required': True}),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+
+        if password1 and password2:
+            if password1 != password2:
+                self.add_error("password2", "Введенные пароли не совпадают.")
+
+            if len(password1) < 3:
+                self.add_error(
+                    "password2",
+                    "Введённый пароль слишком короткий. Он \
+                        должен содержать как минимум 3 символа.",
+                )
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get("password1")
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
         
 
 class UserLoginForm(AuthenticationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
-
-    class Meta:
-        model = User
-        fields = ['username', 'password']
+    username = forms.CharField(
+        label='Имя пользователя',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    password = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
 
 
 class UserUpdateForm(UserCreateForm):
--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,6 +1,19 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
+    first_name = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name='Фамилия'
+    )
+    USERNAME_FIELD = 'username'
+
     def __str__(self):
-        return self.get_full_name()
+        return f'{self.first_name} {self.last_name}'
